22ee16caae6faabb308e9ef2ecf43ffec45a2a62
Paola Santillan <psantillan@indec.gob.ar>
2020-11-25 10:28:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Axios from 'axios';
+import Test from './components/Test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const listUsers = async () => {
+  const {data, status} = await Axios.get('http://localhost:3001/api/routeexample');
+  const infoAxios = await Axios.get('http://localhost:3001/api/routeexample');
+  console.log(data);
+  console.log(status);
+  console.log(infoAxios);
+  if(status !== 200){
+    return [];
+  }
+  return data;
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      documents: [],
+      headers: [
+        {
+          label: 'Nombre',
+          key: 'name'
+        }
+      ]
+    }
+  }
+
+  async componentDidMount(){
+    const documents = await listUsers();
+    this.setState({documents});
+  }
+
+  render(){
+    
+    const {documents, headers} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <title>Test</title>
+          <div>
+            esto es un test
+          </div>
+        </header>
+        <div>
+          <Test />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
